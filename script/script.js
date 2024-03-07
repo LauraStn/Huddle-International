d044d6c06e968013266008169fca2c92ea64f8f3
steve <stevechap405@gmail.com>
2024-03-07 14:54:38 +0100

    steve

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ buttonDark.addEventListener("click", () => {
     buttonDark = document.querySelector(".myImg").src = "./image/light-mode.png";
     logo.style.backgroundColor = "white";
     for(let i = 0; i < sectionArticleIllustration.length; i++){
-      sectionArticleIllustration[i].style.backgroundColor = "lightgray"
+      sectionArticleIllustration[i].style.backgroundColor = "red"
     }
     for(let i = 0; i < sectionArticleIllustrationImg.length; i++){
       sectionArticleIllustrationImg[i].style.filter = "brightness(2)"
